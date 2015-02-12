52b6813a01a958efdb9bfc3b4c1a3d72bfe03536
Charles Painter <cpainter85@gmail.com>
2015-02-12 13:38:32 -0700

    Add duedate to tasks, on new page, edit page, show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211214700) do
+ActiveRecord::Schema.define(version: 20150212202603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150211214700) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.boolean  "complete"
+    t.date     "due_date"
   end
 
 end
