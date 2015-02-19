a33a468dfff391df3ada36843ce0f21011b34bd0
Charles Painter <cpainter85@gmail.com>
2015-02-19 16:59:29 -0700

    Edit on index page capitalized

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212223652) do
+ActiveRecord::Schema.define(version: 20150219001133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20150212223652) do
     t.string "question"
     t.string "answer"
     t.string "slug"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
