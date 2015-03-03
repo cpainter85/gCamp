48cd8f84ecdaf05804fa0782aa0e1a0fb846c84a
Charles Painter <cpainter85@gmail.com>
2015-03-03 15:31:53 -0700

    add specs for sign up and sign in

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature 'User can sign in with valid credentials' do
+
+  before :each do
+    @user = User.create(first_name: 'Matt',
+                  last_name: 'Murdock',
+                  email: 'iamnotdaredevil@email.com',
+                  password: 'bornagain')
+  end
+
+  scenario 'User can sign in with valid credentials' do
+    visit root_path
+    expect(page).to have_content 'Sign In'
+    expect(page).to have_no_content 'Sign Out'
+
+    click_link 'Sign In'
+    expect(current_path).to eq '/sign-in'
+    expect(page).to have_content 'Sign into gCamp'
+
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Sign In'
+
+    expect(current_path).to eq '/'
+    expect(page).to have_content 'You have successfully signed in'
+    expect(page).to have_content 'Matt Murdock'
+    expect(page).to have_content 'Sign Out'
+  end
+
+  scenario 'User cannot sign in with invalid credentials' do
+    visit root_path
+    click_link 'Sign In'
+    click_button 'Sign In'
+
+    expect(current_path).to eq '/sign-in'
+    expect(page).to have_no_content 'You have successfully signed in'
+    expect(page).to have_content 'Email / Password combination is invalid'
+    expect(page).to have_no_content 'Sign Out'
+  end
+end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'User can signup' do
+  scenario 'allow a user to sign up with valid credentials' do
+    visit root_path
+    expect(page).to have_content 'Sign Up'
+    expect(page).to have_no_content 'Sign Out'
+
+    click_link 'Sign Up'
+    expect(current_path).to eq '/sign-up'
+    expect(page).to have_content 'Sign up for gCamp!'
+
+    fill_in 'First name', with: 'Wally'
+    fill_in 'Last name', with: 'West'
+    fill_in 'Email', with: 'theflash@email.com'
+    fill_in 'Password', with: 'fastestmanalive'
+    fill_in 'Password confirmation', with: 'fastestmanalive'
+    click_button 'Sign Up'
+
+    expect(current_path).to eq '/'
+    expect(page).to have_content 'You have successfully signed up'
+    expect(page).to have_content 'Wally West'
+    expect(page).to have_content 'Sign Out'
+  end
+
+  scenario 'User cannot sign up with invalid credentials' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
+
+    expect(current_path). to eq '/sign-up'
+    expect(page).to have_no_content 'You have successfully signed up'
+    expect(page).to have_content 'First name can\'t be blank'
+    expect(page).to have_content 'Last name can\'t be blank'
+    expect(page).to have_content 'Email can\'t be blank'
+    expect(page).to have_content 'Password can\'t be blank'
+    expect(page).to have_no_content 'Sign Out'
+
+  end
+
+
+end
