afd8d7f3a506713058db6a5f730128a73af1ab0b
Charles Painter <cpainter85@gmail.com>
2015-03-02 20:46:26 -0700

    Users can sign in

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,4 +3,19 @@ class AuthenticationController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path, notice: 'You have successfully signed out'
   end
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'You have successfully signed in'
+    else
+      flash[:notice] = 'Username / password combination is invalid'
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
    get 'sign-up', to: 'registrations#new'
    post 'sign-up', to: 'registrations#create'
    get 'sign-out', to: 'authentication#destroy'
+   get 'sign-in', to: 'authentication#new'
+   post 'sign-in', to: 'authentication#create'
 
    resources :tasks
    resources :users
