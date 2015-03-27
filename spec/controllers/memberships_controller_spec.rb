b0eecbab6301850d6f009a7d6171e32912b9d7f0
Charles Painter <cpainter85@gmail.com>
2015-03-26 20:55:00 -0600

    started memberships controller

---
+++ spec/controllers/memberships_controller_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+describe MembershipsController do
+  describe 'GET #index' do
+    it 'should have a new membership object' do
+      user = create_user
+      session[:user_id] = user.id
+      project = create_project
+      membership = create_membership(user, project)
+
+      get :index, project_id: project.id
+
+      expect(assigns(:membership)).to be_a_new(Membership)
+
+    end
+  end
+
+  describe 'GET #create' do
+    it 'creates a new membership if user is an owner' do
+      user = create_user
+      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+      session[:user_id] = user.id
+      project = create_project
+      membership = create_membership(user, project, role_id: 2)
+
+      expect {
+        post :create, project_id: project.id,
+        membership: {user_id: user2.id, project_id: project.id, role_id: 1}
+      }.to change { Membership.all.count }.by(1)
+
+      membership = Membership.last
+      expect(membership.user_id).to eq user2.id
+      expect(membership.role_id).to eq 1
+      expect(membership.project_id).to eq project.id
+      expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully added"
+      expect(response).to redirect_to project_memberships_path(project)
+    end
+    it 'creates a new membership if user is an admin' do
+      user = create_user(admin: true)
+      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+      session[:user_id] = user.id
+      project = create_project
+
+      expect {
+        post :create, project_id: project.id,
+        membership: {user_id: user2.id, project_id: project.id, role_id: 1}
+      }.to change { Membership.all.count }.by(1)
+
+      membership = Membership.last
+      expect(membership.user_id).to eq user2.id
+      expect(membership.role_id).to eq 1
+      expect(membership.project_id).to eq project.id
+      expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully added"
+      expect(response).to redirect_to project_memberships_path(project)
+    end
+    it 'redirects a member who is not an owner or admin to projects path' do
+      user = create_user
+      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+      session[:user_id] = user.id
+      project = create_project
+      membership = create_membership(user, project)
+
+      expect {
+        post :create, project_id: project.id,
+        membership: {user_id: user2.id, project_id: project.id, role_id: 1}
+      }.to_not change { Membership.all.count }
+
+      expect(flash[:alert]).to eq 'You do not have access'
+      expect(response).to redirect_to project_path(project)
+    end
+  end
+end
