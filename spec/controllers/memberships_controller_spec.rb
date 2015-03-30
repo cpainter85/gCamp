b5aa068f0d90a38cc187cbc037cf100256130d6d
Charles Painter <cpainter85@gmail.com>
2015-03-30 17:10:06 -0600

    begin refactoring membership controller

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -1,136 +1,199 @@
 require 'rails_helper'
 
 describe MembershipsController do
-  describe 'GET #index' do
-    it 'should have a new membership object' do
-      user = create_user
-      session[:user_id] = user.id
-      project = create_project
-      membership = create_membership(user, project)
 
-      get :index, project_id: project.id
+  let(:user) { create_user }
+  let(:admin) { create_user(
+    first_name: 'Wilson',
+    last_name: 'Fisk',
+    email: 'kingpin@email.com',
+    password: 'kingpinofcrime',
+    admin: true) }
+  let(:user2) { create_user(first_name: 'Walter',
+    last_name: 'White',
+    email: 'breakingbad@email.com',
+    password: 'heisenberg') }
+  let(:project) { create_project }
+  let(:membership) { create_membership(user, project) }
 
-      expect(assigns(:membership)).to be_a_new(Membership)
-
+  describe 'if user is not signed in' do
+    all_pages = ['get :index, project_id: project.id',
+       'post:create, project_id: project.id',
+       'patch :update, project_id: project.id, id: membership.id',
+       'delete :destroy, project_id: project.id, id: membership.id']
+    all_pages.each do |page|
+      it "redirects to sign in page with an error message" do
+        eval(page)
+        expect(response).to redirect_to sign_in_path
+        expect(flash[:alert]).to eq('You must sign in')
+      end
     end
   end
 
-  describe 'GET #create' do
-    it 'creates a new membership if user is an owner' do
-      user = create_user
-      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
-      session[:user_id] = user.id
-      project = create_project
-      membership = create_membership(user, project, role_id: 2)
-
-      expect {
-        post :create, project_id: project.id,
-        membership: {user_id: user2.id, project_id: project.id, role_id: 1}
-      }.to change { Membership.all.count }.by(1)
-
-      membership = Membership.last
-      expect(membership.user_id).to eq user2.id
-      expect(membership.role_id).to eq 1
-      expect(membership.project_id).to eq project.id
-      expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully added"
-      expect(response).to redirect_to project_memberships_path(project)
+  # describe 'if user is admin' do
+  RSpec.shared_examples 'if user is admin' do
+    before :each do
+      session[:user_id] = admin.id
     end
-    it 'creates a new membership if user is an admin' do
-      user = create_user(admin: true)
-      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
-      session[:user_id] = user.id
-      project = create_project
 
-      expect {
-        post :create, project_id: project.id,
-        membership: {user_id: user2.id, project_id: project.id, role_id: 1}
-      }.to change { Membership.all.count }.by(1)
-
-      membership = Membership.last
-      expect(membership.user_id).to eq user2.id
-      expect(membership.role_id).to eq 1
-      expect(membership.project_id).to eq project.id
-      expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully added"
-      expect(response).to redirect_to project_memberships_path(project)
+    describe 'GET #index' do
+      it 'should have a new membership object' do
+        get :index, project_id: project.id
+        expect(assigns(:membership)).to be_a_new(Membership)
+      end
     end
-    it 'redirects a member who is not an owner or admin to projects path' do
-      user = create_user
-      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+
+    describe 'POST #create' do
+      it 'creates a new membership' do
+
+        expect {
+          post :create, project_id: project.id,
+          membership: {user_id: user2.id, project_id: project.id, role_id: 1}
+        }.to change { Membership.all.count }.by(1)
+
+        membership = Membership.last
+        expect(membership.user_id).to eq user2.id
+        expect(membership.role_id).to eq 1
+        expect(membership.project_id).to eq project.id
+        expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully added"
+        expect(response).to redirect_to project_memberships_path(project)
+      end
+    end
+
+    describe 'PATCH #update' do
+      it 'allows admin to update membership' do
+        membership2 = create_membership(user2, project)
+        expect {
+          patch :update, project_id: project.id, id: membership2.id,
+          membership: {role_id: 2}
+        }.to change { membership2.reload.role_id}.from(1).to(2)
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'allows admin to destroy membership' do
+        membership2 = create_membership(user2, project)
+
+        expect {
+          delete :destroy, project_id: project.id, id: membership2.id
+        }.to change { Membership.all.count }.by(-1)
+
+        expect(flash[:notice]).to eq "#{membership2.user.full_name} was successfully removed"
+        expect(response).to redirect_to project_memberships_path(project)
+      end
+    end
+  end
+
+  describe 'if user is owner' do
+    before :each do
+      membership.update_attributes(role_id: 2)
       session[:user_id] = user.id
-      project = create_project
-      membership = create_membership(user, project)
-
-      expect {
-        post :create, project_id: project.id,
-        membership: {user_id: user2.id, project_id: project.id, role_id: 1}
-      }.to_not change { Membership.all.count }
-
-      expect(flash[:alert]).to eq 'You do not have access'
-      expect(response).to redirect_to project_path(project)
+    end
+    describe 'all pages' do
+      it_behaves_like 'if user is admin'
     end
   end
 
-  describe 'GET #destroy' do
-    it 'allows owner to destroy membership' do
-      user = create_user
-      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
-      session[:user_id] = user.id
-      project = create_project
-      membership = create_membership(user, project, role_id: 2)
-      membership2 = create_membership(user2, project)
 
-      expect {
-        delete :destroy, project_id: project.id, id: membership2.id
-      }.to change { Membership.all.count }.by(-1)
+  # describe 'GET #index' do
+  #   it 'should have a new membership object' do
+  #     user = create_user
+  #     session[:user_id] = user.id
+  #     project = create_project
+  #     membership = create_membership(user, project)
+  #
+  #     get :index, project_id: project.id
+  #
+  #     expect(assigns(:membership)).to be_a_new(Membership)
+  #
+  #   end
+  # end
+  #
+  # describe 'GET #create' do
+  #   it 'creates a new membership if user is an owner' do
+  #     user = create_user
+  #     user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+  #     session[:user_id] = user.id
+  #     project = create_project
+  #     membership = create_membership(user, project, role_id: 2)
+  #
+  #     expect {
+  #       post :create, project_id: project.id,
+  #       membership: {user_id: user2.id, project_id: project.id, role_id: 1}
+  #     }.to change { Membership.all.count }.by(1)
+  #
+  #     membership = Membership.last
+  #     expect(membership.user_id).to eq user2.id
+  #     expect(membership.role_id).to eq 1
+  #     expect(membership.project_id).to eq project.id
+  #     expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully added"
+  #     expect(response).to redirect_to project_memberships_path(project)
+  #   end
 
-      expect(flash[:notice]).to eq "#{membership2.user.full_name} was successfully removed"
-      expect(response).to redirect_to project_memberships_path(project)
+  #   it 'redirects a member who is not an owner or admin to projects path' do
+  #     user = create_user
+  #     user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+  #     session[:user_id] = user.id
+  #     project = create_project
+  #     membership = create_membership(user, project)
+  #
+  #     expect {
+  #       post :create, project_id: project.id,
+  #       membership: {user_id: user2.id, project_id: project.id, role_id: 1}
+  #     }.to_not change { Membership.all.count }
+  #
+  #     expect(flash[:alert]).to eq 'You do not have access'
+  #     expect(response).to redirect_to project_path(project)
+  #   end
+  # end
+  #
+  # describe 'GET #destroy' do
+  #   it 'allows owner to destroy membership' do
+  #     user = create_user
+  #     user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+  #     session[:user_id] = user.id
+  #     project = create_project
+  #     membership = create_membership(user, project, role_id: 2)
+  #     membership2 = create_membership(user2, project)
+  #
+  #     expect {
+  #       delete :destroy, project_id: project.id, id: membership2.id
+  #     }.to change { Membership.all.count }.by(-1)
+  #
+  #     expect(flash[:notice]).to eq "#{membership2.user.full_name} was successfully removed"
+  #     expect(response).to redirect_to project_memberships_path(project)
+  #
+  #   end
 
-    end
-    it 'allows admin to destroy membership' do
-      user = create_user(admin: true)
-      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
-      session[:user_id] = user.id
-      project = create_project
-      membership = create_membership(user2, project)
-
-      expect {
-        delete :destroy, project_id: project.id, id: membership.id
-      }.to change { Membership.all.count }.by(-1)
-
-      expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully removed"
-      expect(response).to redirect_to project_memberships_path(project)
-
-    end
-    it 'allows member to destroy their own membership' do
-      user = create_user
-      session[:user_id] = user.id
-      project = create_project
-      membership = create_membership(user, project)
-
-      expect {
-        delete :destroy, project_id: project.id, id: membership.id
-      }.to change { Membership.all.count }.by(-1)
-
-      expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully removed"
-      expect(response).to redirect_to projects_path
-
-    end
-    it 'does not allow member to destroy someone else\'s membership\'s' do
-      user = create_user
-      session[:user_id] = user.id
-      user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
-      project = create_project
-      membership = create_membership(user, project)
-      membership2 = create_membership(user2, project)
-
-      expect {
-        delete :destroy, project_id: project.id, id: membership2.id
-      }.to_not change { Membership.all.count }
-
-      expect(flash[:alert]).to eq "You do not have access"
-      expect(response).to redirect_to project_path(project)
-
-    end
-  end
+  #   it 'allows member to destroy their own membership' do
+  #     user = create_user
+  #     session[:user_id] = user.id
+  #     project = create_project
+  #     membership = create_membership(user, project)
+  #
+  #     expect {
+  #       delete :destroy, project_id: project.id, id: membership.id
+  #     }.to change { Membership.all.count }.by(-1)
+  #
+  #     expect(flash[:notice]).to eq "#{membership.user.full_name} was successfully removed"
+  #     expect(response).to redirect_to projects_path
+  #
+  #   end
+  #   it 'does not allow member to destroy someone else\'s membership\'s' do
+  #     user = create_user
+  #     session[:user_id] = user.id
+  #     user2 = create_user(first_name: 'Walter', last_name: 'White', email: 'breakingbad@email.com', password: 'heisenberg')
+  #     project = create_project
+  #     membership = create_membership(user, project)
+  #     membership2 = create_membership(user2, project)
+  #
+  #     expect {
+  #       delete :destroy, project_id: project.id, id: membership2.id
+  #     }.to_not change { Membership.all.count }
+  #
+  #     expect(flash[:alert]).to eq "You do not have access"
+  #     expect(response).to redirect_to project_path(project)
+  #
+  #   end
+  # end
 end
