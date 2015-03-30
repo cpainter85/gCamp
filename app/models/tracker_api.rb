7cab5d92ce6b6f0c0cd1301afb4c3b10967c088e
Charles Painter <cpainter85@gmail.com>
2015-03-30 14:35:42 -0600

    handles invalid tracker tokens

--- app/models/tracker_api.rb
+++ app/models/tracker_api.rb
@@ -20,4 +20,13 @@ class TrackerAPI
     end
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def authenticate_token(token)
+    response = @conn.get do |request|
+      request.url "/services/v5/me"
+      request.headers['Content-Type'] = 'application/json'
+      request.headers['X-TrackerToken'] = token
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
